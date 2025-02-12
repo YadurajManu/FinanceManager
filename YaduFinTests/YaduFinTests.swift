501c8b6da214e77c4f94b5a15ed8a353299736ac
YaduLuxe <yadurajsingham@gmail.com>
2025-02-12 17:11:25 +0530

    First

---
+++ YaduFinTests/YaduFinTests.swift
@@ -0,0 +1,17 @@
+//
+//  YaduFinTests.swift
+//  YaduFinTests
+//
+//  Created by Yaduraj Singh on 25/12/24.
+//
+
+import Testing
+@testable import YaduFin
+
+struct YaduFinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
